91d2bd72775be790ca977ae5cee20b739812813d
Sophie Spang <soph8111@stud.kea.dk>
2021-08-31 13:59:56 +0200

    Added Comments to the code

--- typewriter.js
+++ typewriter.js
@@ -12,18 +12,21 @@ init();
 function init() {
   maxNumbersOfIterations = typewriter.length; // Loopet skal køre det antal gange, som teksten indholder karakterer.
   console.log(maxNumbersOfIterations); // 44 gange
-  iterator = 0;
-  loop();
+  iterator = 0; // Den skal starte med færste bogstav, hvilket har nummer 1.
+  loop(); // Kalder på  loopet
 }
 
 function loop() {
-  console.log("Loop", iterator);
+  console.log("Loop", iterator); // Iterator viser hvilket nummer bogstav/karakter vi arbejder med
   iterator++; // Plusser én til hver gang loopet har kørt
 
+  // Så længe iterator er mindre end eller lig med antallet af bogstaver/karaktere
   if (iterator <= maxNumbersOfIterations) {
-    setTimeout(loop, 650);
+    setTimeout(loop, 650); // Sætter delay på loopet (0,65 sekunder)
   }
   output = typewriter.substring(iterator, 0);
-  console.log(output);
-  document.querySelector("#typewriter").textContent = output;
+  // ^^ Med substing laver vi en ny string, og med paranteserne vælger vi hvilket tegn fra den nuværende teskt der skal udskrives.
+  // ^^ Laver substring og udskriver til og med det tegn vi er kommet til. Vi siger den skal "stoppe" ved 0, da vi gerne vil have udskrevet alt det inden det aktuelle tegn også.
+  console.log(output); // Udskriver den aktuelle tekst i konsollen
+  document.querySelector("#typewriter").textContent = output; // Sætter h1 = output
 }
